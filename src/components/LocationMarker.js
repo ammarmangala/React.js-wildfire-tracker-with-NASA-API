8139e05e01bd9ecf3dce1a16fb7a93546d956c68
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2020-12-22 19:52:00 +0100

    update

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -4,7 +4,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert'
 const LocationMarker = ({ lat, lng, onClick }) => {
     return (
         <div className="location-marker" onClick={onClick} >
-            <Icon icon={locationIcon} className="location-con"/>
+            <Icon icon={locationIcon} className="location-icon"/>
         </div>
     );
 };
